c66272505d795f2aec6070b6292d1869dca1e055
priest-2105 <fawzybailey782@gmail.com>
2024-08-01 02:11:40 +0100

    firebase setup ✨✨

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,13 @@ import "firebase/compat/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyC5uvEVrMvaEZzcO7QrV8u8oJZQXbgF6gs",
-	authDomain: "shewin-40205.firebaseapp.com",
-	projectId: "shewin-40205",
-	storageBucket: "shewin-40205.appspot.com",
-	messagingSenderId: "604340215105",
-	appId: "1:604340215105:web:082a7c4ea18084346515c0",
+	apiKey: "AIzaSyCuGdhX0fKLElXvHQ7QDxnCe4zaUjhe9SU",
+	authDomain: "xelle-bc603.firebaseapp.com",
+	projectId: "xelle-bc603",
+	storageBucket: "xelle-bc603.appspot.com",
+	messagingSenderId: "3339921783",
+	appId: "1:3339921783:web:25296c3c3738bfaa795972",
+	measurementId: "G-070GW3LZWQ"
 };
 
 // Initialize Firebase
